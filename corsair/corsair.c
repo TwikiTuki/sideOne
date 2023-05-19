c2dfb48a1b78deea58f4ab37f451f854b874a9e1
Joan Renau valls <jrenau-v@car2s2.42barcelona.com>
2023-05-19 20:07:03 +0200

    some commets to corsair

--- corsair/corsair.c
+++ corsair/corsair.c
@@ -14,7 +14,9 @@ int main(void)
 	BIGNUM			*res;			//
 	int				result;
 	BN_CTX			*ctx;			//
+	// Inicialitza el BIGNUMBER res
 	res = BN_new();
+	// Inicialitza el BN_CTX ctx (ctxe serveis per algunes operacions amb BIGNUMBERS com la multiplicacio)
 	ctx = BN_CTX_new();
 
 	fp = fopen("challenge_corsair/sample/public.pem", "r");
@@ -25,28 +27,34 @@ int main(void)
 	}
 	printf("file pointer: %p\n", fp);
 	twk_rsa = NULL;
+	// Carrega la clau publica
 	twk_rsa_result = PEM_read_RSA_PUBKEY(fp, &twk_rsa, NULL, NULL);
 	printf("%p\n", &twk_rsa);
 	printf("Result: %p %p\n", twk_rsa, twk_rsa_result);
+	// impirmexi el modulus de la clau publica
 	n = RSA_get0_e(twk_rsa_result);
 	printf("sdaf\n");
+	// imprimeix el exponent de la clau publica
 	e = RSA_get0_n(twk_rsa_result);
 	printf("fasd\n");
 	write(1,"vals:\n", 5);
 	write(1,"\n", 1);printf("\n\nn:  ");
 	BN_print_fp(stdout, n);
 	write(1,"\n", 1);printf("\n\ne:  ");
+	// imprimeix el BIGNUMBER e (exponent)
 	BN_print_fp(stdout, e);
+	// suma els BIGNUMBERs e n
 	result = BN_add(res, e, n);
 	write(1,"\n", 1);printf("\n\nres:  ");
 	BN_print_fp(stdout, res);
 
 	printf("\n-------------------------\n");
+	// multiplica els BIGNUMBER n e
 	result = BN_mul(res, n, e, ctx);	
 	write(1,"\n", 1);printf("\n\nres:  ");
 	BN_print_fp(stdout, res);
 	
-		
+	// NO mem leaks please		
 	RSA_free(twk_rsa);
 	RSA_free(twk_rsa_result);
 	BN_free(n);	
